b55ee2d73a385bf018a98375e92691932e5571a2
Ecker Sandor <esanya@gmail.com>
2018-08-18 21:48:00 +0200

    wget + parse

--- relectrl.py
+++ relectrl.py
@@ -1,5 +1,15 @@
+import sys
+import urllib2
 import icalendar
 
+url = sys.argv[1]
 
+response = urllib2.urlopen(url)
+webContent = response.read()
 
+#print(webContent[0:300])
+
+dt = icalendar.Calendar.from_ical(webContent)
+
+print(dt)
 
