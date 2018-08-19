98c89cd33fe2f7d20a996fd5326c00246fd0e2f2
Ecker Sandor <esanya@gmail.com>
2018-08-19 13:20:00 +0200

    read from file

--- relectrl.py
+++ relectrl.py
@@ -2,14 +2,16 @@ import sys
 import urllib2
 import icalendar
 
-url = sys.argv[1]
+if ( len(sys.argv) > 1 ):
+    url = sys.argv[1]
+    response = urllib2.urlopen(url)
+    calendarText = response.read()
+    #print(calendarText[0:300])
+else:
+    calendarText = sys.stdin.read()
 
-response = urllib2.urlopen(url)
-webContent = response.read()
 
-#print(webContent[0:300])
-
-dt = icalendar.Calendar.from_ical(webContent)
+dt = icalendar.Calendar.from_ical(calendarText)
 
 #print(dt)
 
